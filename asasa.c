585583b04fe37d7c7b82071991d9f28245a94ef7
Sanam Acharya <104333903+SanamAcharya@users.noreply.github.com>
2023-06-19 21:37:10 +0545

    Add files via upload

---
+++ asasa.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int a[100],i,n,*ptr,*max;
+
+    printf("ENTER THE NUMBER YOU WANT");
+    scanf("%d",&n);
+
+    printf("ENTER THE NUM");
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    max=&a[0];
+    ptr=&a[0];
+
+
+
+    for(i=0; i<n; i++)
+    {
+        if(*ptr>*max)
+        {
+        max=ptr;
+        }        ptr++;
+
+    }
+
+    printf("THE LARGEST:%d",*max);
+}
