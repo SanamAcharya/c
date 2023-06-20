948ddc9326f613a2d6588a2936a8bd9c518b7471
Sanam Acharya <104333903+SanamAcharya@users.noreply.github.com>
2023-06-20 08:34:28 +0545

    Create sorting_array_element_ascending.c

---
+++ sorting_array_element_ascending.c
@@ -0,0 +1,35 @@
+//ascending order sorting of array
+
+
+#include<stdio.h>
+int main()
+{
+    int a[10],i,j,temp;
+    printf("ENTER 10 NUMBERS\n");
+    for (i=0; i<10; i++)
+    {
+        printf("ENTER no %d:\t",i+1);
+        scanf("%d",&a[i]);
+
+    }
+    for(i=0; i<10; i++)
+{
+    for(j=0; j<10; j++)
+        {
+            if(a[i]<a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+
+
+    printf("ASCENDING ORDER:\n");
+
+
+    for(i=0;i<10;i++){
+    printf("%d\n",a[i]);
+    }
+}
