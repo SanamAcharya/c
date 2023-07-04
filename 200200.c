f6a104177a8c815ca8677d4b84f8c532cd529589
Sanam <heyesanam@gmail.com>
2023-07-04 23:11:55 +0545

    x

---
+++ 200200.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_STR_LENGTH 100
+#define MAX_NUM_STRINGS 10
+
+int main()
+{
+    char a[MAX_NUM_STRINGS][MAX_STR_LENGTH];
+    char temp[MAX_STR_LENGTH];
+    int i, j, n;
+
+    printf("ENTER THE NUMBER OF STRINGS: ");
+    scanf("%d", &n);
+    getchar(); // Clear the newline character from the input buffer
+
+    printf("ENTER %d STRINGS:\n", n);
+    for (i = 0; i < n; i++)
+    {
+        fgets(a[i], MAX_STR_LENGTH, stdin);
+        a[i][strcspn(a[i], "\n")] = '\0'; // Remove the newline character
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = i + 1; j < n; j++)
+        {
+            if (strcmp(a[i], a[j]) > 0)
+            {
+                strcpy(temp, a[i]);
+                strcpy(a[i], a[j]);
+                strcpy(a[j], temp);
+            }
+        }
+    }
+
+    printf("SORTED STRINGS:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%s\n", a[i]);
+    }
+
+    return 0;
+}
---
+++ binary.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int n, r, b = 0, base = 1;
+     printf("enteer");
+    scanf("%d", &n);
+    
+
+    while (n > 0)
+    {
+        r= n % 2;
+        b=b+r*base;
+        n=n/2;
+        base=base*10;
+    }
+    printf("%d",b);
+    return 0;
+}
